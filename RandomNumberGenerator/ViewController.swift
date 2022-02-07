9545567532957d3fa1c33ac13e5448763a54b732
Starsky <starsky.m@live.com>
2022-02-07 17:12:31 +0100

    add outlets and actions

--- RandomNumberGenerator/ViewController.swift
+++ RandomNumberGenerator/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var chooseButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func chooseButtonTapped(_ sender: UIButton) {
+        print("tapped")
+    }
+    
 }
 
